d17136833cc59e6288a33c9f746acf872ad9fa32
Maria Pineda <mariapineda@uan.edu.co>
2017-05-29 23:13:18 -0500

    Actualizacion del proyecto
    Se realiza la actualizacion del proyeco y se sube al repositorio en su
    version funcional

---
+++ src/java/Model/User.java
@@ -0,0 +1,50 @@
+
+package Model;
+
+public class User 
+{
+    private int id;
+    private String user;
+    private String password;
+    private String name;
+    
+
+    public User(){}
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
